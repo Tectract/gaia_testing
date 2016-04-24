d6f93b6039e06e0a8e2ab8159b41969b3947debe
Ryan Molecke <ryan@molecke.com>
2016-04-24 11:03:35 -0600

    updates for initial error handling and promisification

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 var express = require('express');
 var app = express();
-
-app.get('/', function(req, res) {
-  res.send('hello world');
-});
+var Promise = require("bluebird");
+var request = Promise.promisify(require("request"));
 
 app.get('/v1/api/term/:id/longest-preview-media-url', function (req, res) {
-    //var taskId = req.params.id;
-    
-    res.send('params were ' + JSON.stringify(req.params.id) + '\n')
 
+  // res.send('params were ' + JSON.stringify(req.params.id) + '\n')
+  if (isNaN(req.params.id)) {
+    res.status(400).send({error: 'Please supply a numeric id parameter.'});
+  } else {
+    request('http://www.gaia.com/api/vocabulary/1/' + req.params.id, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log(body) // Show the HTML for the Google homepage.
+        res.send(body)
+      } else {
+        res.status(500).send({error: 'Vocabularies api endpoint unavailable.'});
+      }
+    }).then(function () {
+      console.log("next")
+    })
+  }
 });
 
 app.listen(8080);
